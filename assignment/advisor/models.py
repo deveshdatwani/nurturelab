52c4362468b734097102c7ad5edb56b70f3c45c4
Deveshd Datwani <datwanidevesh@gmail.com>
2021-05-02 19:39:12 +0530

    response

--- assignment/advisor/models.py
+++ assignment/advisor/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Advisor(models.Model):
-	name = models.CharField(max_length=30)
-	photo_url = models.CharField(max_length=50)
+	name = models.CharField(max_length=30, null=False)
+	photo_url = models.CharField(max_length=50, null=False)
--- assignment/advisor/views.py
+++ assignment/advisor/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Advisor
 
 # Create your views here.
-def create_advisor(*args, **kwargs):
-	return HttpResponse("<h1>Hello, World!</h1>")
-
+def create_advisor(request,*args, **kwargs):
+	obj = Advisor.objects.get(name="devesh datwani")
+	context = {
+		"obj":obj
+	}
+	return render(context)
