af88494cc667daf8f614ee155b6e2885f5086a60
Deveshd Datwani <datwanidevesh@gmail.com>
2021-05-02 22:25:15 +0530

    user registration

--- assignment/user/views.py
+++ assignment/user/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
+from .forms import UserForm
 # Create your views here.
 
 def register_user(request):
-
+	form = UserForm(request.POST)
+	if form.is_valid():
+		form.save()
 	#u = User.objects.get(id=1)
 
 	return HttpResponse("Hello")
