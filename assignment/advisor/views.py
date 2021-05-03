1a343fea5e56f3ec0676ea1242522e827798460d
Deveshd Datwani <datwanidevesh@gmail.com>
2021-05-03 13:42:02 +0530

    changing name of function

--- assignment/advisor/views.py
+++ assignment/advisor/views.py
@@ -4,7 +4,9 @@ from .models import Advisor
 from .forms import AdvisorForm
 
 # Create your views here.
-def create_advisor(request,*args, **kwargs):
+def register_advisor(request,*args, **kwargs):
+	#inser an advisor in the Advisor table
+	
 	form = AdvisorForm(request.POST or None)
 	
 	if form.is_valid():
@@ -13,3 +15,14 @@ def create_advisor(request,*args, **kwargs):
 
 	else:
 		return HttpResponse("400_BAD_REQUEST")
+
+def get_advisors(request, *args, **kwargs):
+	#fetch all advisors from the Advisor table and list them
+	
+	obj = Advisor.objects.get()
+
+	return HttpResponse("all advisors")
+
+def book_call(request, *args, **kwargs):
+	#insert an entry into the Calls table
+	return HttpResponse("booked your call")
