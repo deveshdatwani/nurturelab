840f9b2fc8c4fc1b6c915c1946a8c33a55b64c23
Deveshd Datwani <datwanidevesh@gmail.com>
2021-05-03 13:23:01 +0530

    adding a calls model

--- assignment/advisor/models.py
+++ assignment/advisor/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Advisor(models.Model):
 	name = models.CharField(max_length=30, null=False)
 	photo_url = models.CharField(max_length=50, null=False)
+
+class Calls(models.Model):
+	advisor = models.CharField(max_length=30, null=False)
+	user = models.CharField(max_length=30, null=False)
+	datetime = models.DateTimeField()
